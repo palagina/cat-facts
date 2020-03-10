178fa8532662a465956c47f3cc4098647d21e2bc
Mariia Palagina <mariia.palagina@softability.fi>
2020-03-10 19:26:17 +0200

    build added

---
+++ build/precache-manifest.c9cb4a46a8addd9e2a0284d34df56854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c096f202b9f9d8900a9c9db6e26f7e56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4a0c2c5fc976110629f",
+    "url": "./static/css/2.a8ba0f7c.chunk.css"
+  },
+  {
+    "revision": "d6299b6c99b24cd0194a",
+    "url": "./static/css/main.6b70c854.chunk.css"
+  },
+  {
+    "revision": "e4a0c2c5fc976110629f",
+    "url": "./static/js/2.26762717.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "./static/js/2.26762717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6299b6c99b24cd0194a",
+    "url": "./static/js/main.6351d698.chunk.js"
+  },
+  {
+    "revision": "53010082b5fd3165a2f6",
+    "url": "./static/js/runtime-main.b347e5b4.js"
+  }
+]);
